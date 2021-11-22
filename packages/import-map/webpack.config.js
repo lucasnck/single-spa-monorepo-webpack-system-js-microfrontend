0a443863d0b5116f05cfbb7780156d567e53994e
Lucas Ribeiro <lucasnck@hotmail.com>
2021-11-21 23:08:51 -0300

    ✅ create pages

---
+++ packages/import-map/webpack.config.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "exm";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "import-map",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin(),
+    ],
+  });
+};
