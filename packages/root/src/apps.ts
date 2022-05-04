9b506268d7c6a5cb26939f16e89899bad9a52cce
Lucas Ribeiro <lucasnck@hotmail.com>
2022-05-03 22:51:59 -0300

    ✅ rename template

--- packages/root/src/apps.ts
+++ packages/root/src/apps.ts
@@ -5,7 +5,7 @@ import { env } from "./utils/env";
 export const apps: IApp[] = [
   {
     type: "application",
-    name: "@exm/template",
+    name: "@exm/layout",
     src: `${env.TEMPLATE_PATH}/exm-template.js`,
   },
   // #/DNC BUILDER_APPS
